9c8f2e60ecefabaae8073708d3b6eb7f049ee88c
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-11-09 15:26:46 -0500

    Player movement, no rotation

---
+++ src/player/mod.rs
@@ -0,0 +1,32 @@
+use bevy::prelude::*;
+
+pub mod components;
+mod systems;
+
+use systems::*;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinedSystemSet;
+
+pub enum PlayerSystemSet {
+    Movement,
+    Confined,
+}
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_player)
+            .add_systems(
+                Update,
+                player_movement
+                    .before(confine_player_movement)
+                    .in_set(MovementSystemSet),
+            )
+            .add_systems(Update, confine_player_movement.in_set(ConfinedSystemSet));
+    }
+}
