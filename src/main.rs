5bb6731bacf6dae92072ad03d1b2277b628b59c3
Alikhan Khakimov <alikhankhakimov@outlook.com>
2024-11-09 15:26:12 -0500

    Player movement, no rotation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod player;
+mod systems;
+
+use bevy::prelude::*;
+use player::PlayerPlugin;
+use systems::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(PlayerPlugin)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, exit_game)
+        .run();
 }
