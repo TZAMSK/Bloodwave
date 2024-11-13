b722a118490ef0a9ac1aa66a4ae1ce8bf40d7633
Alikhan Khakimov <alikhankhakimov@outlook.com>
2024-11-12 20:08:28 -0500

    Enemy implementation that rotate and follow the player, but dont follow and shoot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod bullet;
 mod camera;
+mod enemy;
 mod player;
 mod systems;
 mod world;
@@ -7,17 +8,26 @@ mod world;
 use bevy::prelude::*;
 use bullet::BulletPlugin;
 use camera::CameraPlugin;
+use enemy::EnemyPlugin;
 use player::PlayerPlugin;
 use systems::*;
 use world::WorldPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resizable: false,
+                mode: bevy::window::WindowMode::BorderlessFullscreen,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(WorldPlugin)
         .add_plugins(PlayerPlugin)
         .add_plugins(CameraPlugin)
         .add_plugins(BulletPlugin)
+        .add_plugins(EnemyPlugin)
         .add_systems(Update, exit_game)
         .run();
 }
