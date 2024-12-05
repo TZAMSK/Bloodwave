1abe75de46282239fd671f0d033f89bdec4f68a7
Alikhan Khakimov <alikhankhakimov@outlook.com>
2024-12-04 23:17:51 -0500

    XP system (dropping and movement)

--- src/enemy/components.rs
+++ src/enemy/components.rs
@@ -4,6 +4,16 @@ use bevy::prelude::*;
 pub struct Enemy {
     pub speed: f32,
     pub aggro_range: f32,
+    pub xp: XP,
+    pub level: u32,
+}
+
+#[derive(Component)]
+pub struct XP {
+    pub xp: f32,
+    pub drop_chance: f32,
+    pub distance_pick_up: f32,
+    pub speed: f32,
 }
 
 impl Default for Enemy {
@@ -11,6 +21,19 @@ impl Default for Enemy {
         Self {
             speed: 200.0,
             aggro_range: 400.0,
+            xp: XP::default(),
+            level: 1,
+        }
+    }
+}
+
+impl Default for XP {
+    fn default() -> Self {
+        Self {
+            xp: 5.0,
+            drop_chance: 0.4,
+            distance_pick_up: 50.0,
+            speed: 300.0,
         }
     }
 }
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -14,6 +14,7 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
             .add_systems(Update, player_movement)
-            .add_systems(Update, player_rotate);
+            .add_systems(Update, player_rotate)
+            .add_systems(Update, pick_up_xp);
     }
 }
