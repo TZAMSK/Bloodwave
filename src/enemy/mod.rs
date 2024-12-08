36018755c175dc32e7ee3c6b0b31979d532f76d9
Alikhan Khakimov <alikhankhakimov@outlook.com>
2024-12-08 15:53:29 -0500

    Movement unique to melee enemies

--- src/enemy/mod.rs
+++ src/enemy/mod.rs
@@ -6,13 +6,14 @@ mod types;
 pub mod xp;
 
 use systems::*;
+use types::melee::movement_systems;
 
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_enemy)
-            .add_systems(Update, enemy_movement)
+            .add_systems(Update, movement_systems::enemy_movement)
             .add_systems(Update, correct_collision_enemies);
     }
 }
--- src/enemy/systems.rs
+++ src/enemy/systems.rs
@@ -40,29 +40,6 @@ pub fn spawn_enemy(
     }
 }
 
-pub fn enemy_movement(
-    player_query: Query<&mut Transform, (With<Player>, Without<Enemy>)>,
-    mut enemy_query: Query<(&Enemy, &mut Transform)>,
-    time: Res<Time>,
-) {
-    if let Some(player) = player_query.iter().next() {
-        for (enemy, mut transform_enemy) in enemy_query.iter_mut() {
-            let mut enemy_position = transform_enemy.translation;
-            let player_position = player.translation;
-            let direction = player_position.distance(enemy_position);
-
-            let distance = player_position - enemy_position;
-
-            let angle = distance.y.atan2(distance.x);
-            if direction <= enemy.aggro_range {
-                transform_enemy.rotation = Quat::from_rotation_z(angle);
-                enemy_position += distance.normalize() * enemy.speed * time.delta_seconds();
-                transform_enemy.translation = enemy_position
-            }
-        }
-    }
-}
-
 pub fn correct_collision_enemies(mut enemy_query: Query<(&Enemy, &mut Transform)>) {
     let mut enemy_data: Vec<(Transform, f32)> = Vec::new();
 
