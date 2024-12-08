ece4138d3a47df6ab40e8558c95060a0dd7af9ee
Alikhan Khakimov <alikhankhakimov@outlook.com>
2024-12-08 07:40:19 -0500

    UI for CoreStats(Health, Shield and Stamina) now update with actual player health, shield and stamina

--- src/player/components.rs
+++ src/player/components.rs
@@ -11,9 +11,27 @@ pub struct Player {
 
 #[derive(Component)]
 pub struct CoreStats {
-    pub health: f32,
-    pub shield: f32,
-    pub stamina: f32,
+    pub health: Health,
+    pub shield: Shield,
+    pub stamina: Stamina,
+}
+
+#[derive(Component)]
+pub struct Health {
+    pub current_health: f32,
+    pub max_health: f32,
+}
+
+#[derive(Component)]
+pub struct Shield {
+    pub current_shield: f32,
+    pub max_shield: f32,
+}
+
+#[derive(Component)]
+pub struct Stamina {
+    pub current_stamina: f32,
+    pub max_stamina: f32,
 }
 
 impl Default for Player {
@@ -22,9 +40,18 @@ impl Default for Player {
             size: 500.0,
             speed: 500.0,
             core_stats: CoreStats {
-                health: 100.0,
-                shield: 20.0,
-                stamina: 10.0,
+                health: Health {
+                    current_health: 100.0,
+                    max_health: 100.0,
+                },
+                shield: Shield {
+                    current_shield: 20.0,
+                    max_shield: 20.0,
+                },
+                stamina: Stamina {
+                    current_stamina: 10.0,
+                    max_stamina: 10.0,
+                },
             },
             xp: 0.0,
             level: 1,
